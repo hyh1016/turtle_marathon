214969ae3b943d6563866a846a1f720ddd6c3a25
Yihyun <hihyun16@gmail.com>
2020-09-13 20:32:50 +0900

    feat: judgement tell who is winner 배경음악 추가 심판이 누가 승자인지 화면에 출력

--- turtle_marathon.py
+++ turtle_marathon.py
@@ -1,56 +1,102 @@
 import turtle as t
-player = t
-goal_x = 295
+import pygame as pg
+import time
+
+thema = './thema.wav' # 테스트용 테마 송
+pg.mixer.init()
+pg.mixer.music.load(thema)
+
+player = t.Turtle()
 enemy = t.Turtle()
 judgment = t.Turtle()
+goal_x = 295
+is_play = False
+
+# 플레이어
+player.shape('turtle')
+player.color('blue')
+player.speed(3)
+player.up()
+
+# 적 거북이
+enemy.shape('turtle')
+enemy.color('red')
+enemy.up()
+enemy.speed(3)
+
+# 심판 거북이
+judgment.shape('turtle')
+judgment.color('black')
+judgment.pensize(10)
+judgment.speed(3)
+
+# t(Default): for print message
+t.speed(0)
+t.up()
+t.hideturtle()
 
 def init():
-    # 플레이어
-    player.shape('turtle')
-    player.color('BLUE')
-    player.up()
     player.goto(-300, 200)
 
-    # 적 거북이
-    enemy.shape('turtle')
-    enemy.color('RED')
-    enemy.up()
     enemy.goto(-300, -200)
 
-    # 심판 거북이
-    judgment.shape('turtle')
-    judgment.color('BLACK')
-    judgment.pensize(10)
     judgment.up()
     judgment.goto(300, -300)
     judgment.down()
-    judgment.left(90)
+    judgment.seth(90)
     judgment.forward(600)
-    judgment.left(90)
+
+    pg.mixer.music.play()
+    message("TURTLE MARATHON", "Press [Enter] to start!")
 
 # 이동 함수 - space bar key event와 연결
 def go():
-    player.forward(30) # 플레이어의 이동 속도
+    global is_play
+    if is_play:
+        player.forward(30) # 플레이어의 이동 속도
 
 def goal_in():
     if player.position()[0] >= goal_x:
-        print('You Win!')
+        return 1
     elif enemy.position()[0] >= goal_x:
-        print('You Lose!')
+        return 2
     else: return False
-    return True
 
+def start():
+    t.clear()
+    pg.mixer.music.stop()
+
+    global is_play
+    is_play = True
+
+    while is_play:
+        enemy.forward(1)
+        isGoal = goal_in()
+        if isGoal:
+            who_is_winner(str(isGoal))
+            is_play = False
+            init()
+        
 # 심판 거북이가 누가 이겼는지 직접 말해주는 함수
-def message():
-    pass
+def who_is_winner(winner):
+    judgment_position = judgment.position()
+    t.goto(judgment_position[0] - 50, judgment_position[1])
+    if winner == '1':
+        t.write("you win!", False, "center", ("", 12))
+    elif winner == '2':
+        t.write("you lose!", False, "center", ("", 12))
+    time.sleep(2)
+    t.clear()
+
+def message(m1, m2):
+    t.color('blue')
+    t.goto(0, 100)
+    t.write(m1, False, "center",("",24, "bold"))
+    t.goto(0,-100)
+    t.write(m2, False, "center",("",15, "bold"))
 
 init()
 t.onkey(go, 'space')
+t.onkey(start, '\n')
 t.listen()
-
-while True:
-    enemy.forward(1)
-    if goal_in():
-        break
-
-t.exitonclick()
+t.mainloop()
